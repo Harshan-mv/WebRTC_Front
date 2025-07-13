9ee15f8e7860a166eb3ede5980abcc20b18c212c
HaruDEMO <hv754310@gmail.com>
2025-07-13 23:45:30 +0530

    fetching room on server side

--- src/hooks/useWebRTC.js
+++ src/hooks/useWebRTC.js
@@ -1,4 +1,4 @@
-const Room = require("./models/Room");
+//const Room = require("./models/Room");
 
 let usersInRoom = {};         // roomId: [{ socketId, user }]
 let roomHostMap = {};         // roomId: hostName
